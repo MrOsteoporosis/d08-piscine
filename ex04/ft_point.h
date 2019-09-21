9e48ec75502a0b0793253bfe6d7f77be39eef6a6
Sander Verheijen <sanderverheijen@outlook.com>
2019-09-21 14:07:45 +0200

    ex04, 02/03 bugfixing

---
+++ ex04/ft_point.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_point.h                                         :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: averheij <averheij@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/09/21 13:57:56 by averheij       #+#    #+#                */
+/*   Updated: 2019/09/21 14:01:56 by averheij      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_POINT_H
+# define FT_POINT_H
+typedef struct
+{
+	int x;
+	int y;
+}	t_point;
+#endif
---
+++ ex04/main.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: averheij <averheij@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/09/21 13:58:06 by averheij       #+#    #+#                */
+/*   Updated: 2019/09/21 14:07:02 by averheij      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_point.h"
+
+void	set_point(t_point *point)
+{
+	point->x = 42;
+	point->y = 21;
+}
+
+int		main(void)
+{
+	t_point point;
+
+	set_point(&point);
+	return (0);
+}
