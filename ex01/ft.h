e3bf90a29d721b4bdcfd5a4ffc058b84d12f60ca
Sander Verheijen <sanderverheijen@outlook.com>
2019-09-21 13:19:39 +0200

    ex01

---
+++ ex01/ft.h
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft.h                                               :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: averheij <averheij@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/09/21 13:18:52 by averheij       #+#    #+#                */
+/*   Updated: 2019/09/21 13:19:23 by averheij      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_strlen(char *str);
+void	ft_swap(int *a, int *b);
