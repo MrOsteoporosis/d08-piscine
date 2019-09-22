0abcf028c0695ebd1fc95bece811c65b9c00d1ee
Sander Verheijen <sanderverheijen@outlook.com>
2019-09-22 10:39:18 +0200

    kms

--- ex01/ft.h
+++ ex01/ft.h
@@ -6,7 +6,7 @@
 /*   By: averheij <averheij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/21 13:18:52 by averheij       #+#    #+#                */
-/*   Updated: 2019/09/21 15:17:54 by averheij      ########   odam.nl         */
+/*   Updated: 2019/09/22 10:13:57 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,4 @@ void	ft_putstr(char *str);
 int		ft_strcmp(char *s1, char *s2);
 int		ft_strlen(char *str);
 void	ft_swap(int *a, int *b);
-#endif
+#endif 
